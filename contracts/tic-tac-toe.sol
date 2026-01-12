c9c7be4364bc5d8b451057925c2900dd06d77234
narekokr <narek.okroyan@outlook.com>
2026-01-12 16:08:18 +0100

    add initial contract

---
+++ contracts/tic-tac-toe.sol
@@ -0,0 +1,86 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract TicTacToe {
+    address public player1;
+    address public player2;
+    address public activePlayer;
+    bool public gameStarted;
+    bool public gameActive;
+    
+    // Board: 0 = empty, 1 = Player 1 (X), 2 = Player 2 (O)
+    uint8[9] public board;
+
+    event PlayerJoined(address player, uint8 playerNumber);
+    event MoveMade(address player, uint8 position);
+    event GameOver(address winner);
+
+    // No arguments needed in the constructor anymore
+    constructor() {}
+
+    // Players call this to join the game
+    function joinGame() public {
+        require(!gameStarted, "Game already full");
+        require(msg.sender != player1, "Already joined");
+
+        if (player1 == address(0)) {
+            player1 = msg.sender;
+            emit PlayerJoined(msg.sender, 1);
+        } else {
+            player2 = msg.sender;
+            gameStarted = true;
+            gameActive = true;
+            activePlayer = player1; // Player 1 starts
+            emit PlayerJoined(msg.sender, 2);
+        }
+    }
+
+    function makeMove(uint8 position) public {
+        require(gameActive, "Game is not active");
+        require(msg.sender == activePlayer, "Not your turn");
+        require(position < 9, "Invalid position");
+        require(board[position] == 0, "Spot already taken");
+
+        board[position] = (msg.sender == player1) ? 1 : 2;
+        emit MoveMade(msg.sender, position);
+
+        if (checkWinner()) {
+            gameActive = false;
+            emit GameOver(msg.sender);
+        } else if (isBoardFull()) {
+            gameActive = false;
+            emit GameOver(address(0)); // Draw
+        } else {
+            activePlayer = (activePlayer == player1) ? player2 : player1;
+        }
+    }
+
+    function checkWinner() private view returns (bool) {
+        uint8[3][8] memory winPatterns = [
+            [0, 1, 2], [3, 4, 5], [6, 7, 8],
+            [0, 3, 6], [1, 4, 7], [2, 5, 8],
+            [0, 4, 8], [2, 4, 6]
+        ];
+        uint8 playerVal = (activePlayer == player1) ? 1 : 2;
+
+        for (uint i = 0; i < 8; i++) {
+            if (board[winPatterns[i][0]] == playerVal &&
+                board[winPatterns[i][1]] == playerVal &&
+                board[winPatterns[i][2]] == playerVal) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function isBoardFull() private view returns (bool) {
+        for (uint i = 0; i < 9; i++) {
+            if (board[i] == 0) return false;
+        }
+        return true;
+    }
+
+    function getBoard() public view returns (uint8[9] memory) {
+        return board;
+    }
+}
